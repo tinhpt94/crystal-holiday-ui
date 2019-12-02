49be469e6a61c6842b78e793dbcb43cb6238303f
Pham Tinh <phamtinh@Phams-MacBook-Pro-3.local>
2019-12-02 08:55:53 +0700

    Update customer, product view

--- src/pages/CustomerDetailPage.js
+++ src/pages/CustomerDetailPage.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
-import { Descriptions } from "antd";
+import { Descriptions, Card } from "antd";
 import SiteWrapper from "../components/shared/SiteWrapper";
-import { Page, Grid, Card } from "tabler-react";
+import { Page, Grid, StatsCard } from "tabler-react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from "../actions";
@@ -20,28 +20,59 @@ class CustomerDetailPage extends Component {
     let customer = this.props.customer;
     return (
       <SiteWrapper>
-        <Page.Content title="Customer Dashboard">
+        <Page.Content title="">
           <Grid.Row cards deck>
             <Grid.Col width={12}>
               <Card>
                 {customer && (
-                  <Descriptions title="User Info">
-                    <Descriptions.Item label="UserName">
+                  <Descriptions title="Thông tin khách hàng">
+                    <Descriptions.Item label="Họ tên">
                       {customer.name}
                     </Descriptions.Item>
-                    <Descriptions.Item label="Telephone">
+                    <Descriptions.Item label="Điện thoại">
                       {customer.phone}
                     </Descriptions.Item>
-                    {/* <Descriptions.Item label="Live">
-                      Hangzhou, Zhejiang
+                    <Descriptions.Item label="Trạng thái">
+                      Active
                     </Descriptions.Item>
-                    <Descriptions.Item label="Remark">empty</Descriptions.Item>
-                    <Descriptions.Item label="Address">
+                    <Descriptions.Item label="Địa chỉ">
                       {customer.address1}
-                    </Descriptions.Item> */}
+                    </Descriptions.Item>
                   </Descriptions>
                 )}
               </Card>
+            </Grid.Col>
+            <Grid.Col width={6} sm={6} lg={3}>
+              <StatsCard
+                layout={1}
+                movement={0}
+                total="43"
+                label="Tổng số hợp đồng"
+              />
+            </Grid.Col>
+            <Grid.Col width={6} sm={6} lg={3}>
+              <StatsCard
+                layout={1}
+                movement={6}
+                total="500000000"
+                label="Tổng giá trị hợp đồng"
+              />
+            </Grid.Col>
+            <Grid.Col width={6} sm={6} lg={3}>
+              <StatsCard
+                layout={1}
+                movement={0}
+                total="200000000"
+                label="Đã thanh toán"
+              />
+            </Grid.Col>
+            <Grid.Col width={6} sm={6} lg={3}>
+              <StatsCard
+                layout={1}
+                movement={6}
+                total="300000000"
+                label="Chưa thanh toán"
+              />
             </Grid.Col>
           </Grid.Row>
         </Page.Content>
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -4,80 +4,76 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from "../actions";
 
-import {
-  Page,
-  Grid,
-  Card,
-  } from "tabler-react";
+import { Page, Grid, Card } from "tabler-react";
 
-import {Table} from 'antd'
+import { Table } from "antd";
 
 const columns = [
   {
-    title: 'Tên chương trình',
-    dataIndex: 'name',
-    key: 'name',
+    title: "Tên chương trình",
+    dataIndex: "name",
+    key: "name"
   },
   {
-    title: 'Loại thẻ',
-    dataIndex: 'cardType',
-    key: 'cardType',
+    title: "Loại thẻ",
+    dataIndex: "cardType",
+    key: "cardType"
   },
   {
-    title: 'Mùa du lịch',
-    dataIndex: 'seasonType',
-    key: 'seasonType',
+    title: "Mùa du lịch",
+    dataIndex: "seasonType",
+    key: "seasonType"
   },
   {
-    title: 'Số phòng',
-    dataIndex: 'roomType',
-    key: 'roomType',
+    title: "Số phòng",
+    dataIndex: "roomType",
+    key: "roomType"
   },
   {
-    title: 'Đơn giá / 01 đêm',
-    dataIndex: 'unitPrice',
-    key: 'unitPrice',
+    title: "Đơn giá / 01 đêm",
+    dataIndex: "unitPrice",
+    key: "unitPrice"
   },
   {
-    title: 'Giá trị thẻ',
-    dataIndex: 'cardValue',
-    key: 'cardValue',
+    title: "Giá trị thẻ",
+    dataIndex: "cardValue",
+    key: "cardValue"
   },
   {
-    title: 'Phí thường niên',
-    dataIndex: 'annualFee',
-    key: 'annualFee',
+    title: "Phí thường niên",
+    dataIndex: "annualFee",
+    key: "annualFee"
   },
   {
-    title: 'Phí đăng ký lại / lần',
-    dataIndex: 'reSignFee',
-    key: 'reSignFee',
-  },
-]
+    title: "Phí đăng ký lại / lần",
+    dataIndex: "reSignFee",
+    key: "reSignFee"
+  }
+];
 
-const mapProductToDataSource = (product) => ({
+const mapProductToDataSource = product => ({
   name: product.name,
   cardType: product.cardType.name,
   seasonType: product.seasonType.name,
   roomType: product.roomType.name,
   unitPrice: product.unitPrice,
-  cardValue: 7*product.unitPrice*product.cardType.quantity,
+  cardValue: 7 * product.unitPrice * product.cardType.quantity,
   annualFee: product.annualFee,
   reSignFee: product.reSignFee
-})
+});
 
 class ProductPage extends Component {
-
   componentDidMount = () => {
     this.props.actions.fetchProductRequest();
-  }
+  };
 
-  shouldComponentUpdate = (nextProps) => {
-    return nextProps.products != this.props.products
-  }
+  shouldComponentUpdate = nextProps => {
+    return nextProps.products != this.props.products;
+  };
 
   render() {
-    let dataSource = this.props.products && this.props.products.map(mapProductToDataSource);
+    let dataSource =
+      this.props.products && this.props.products.map(mapProductToDataSource);
     return (
       <SiteWrapper>
         <Page.Content title="Product Dashboard">
